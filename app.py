7aa24c4ff0b076a86bd29cba2e57ea066c03a1fd
voynow <voynow99@gmail.com>
2023-08-28 07:44:51 -0400

    Streaming responses 🎉!

--- app.py
+++ app.py
@@ -3,12 +3,27 @@ from llm_blocks import block_factory
 
 PAGE_TITLE = "🧪🤖 Chat using LLM Blocks"
 USER_ROLE = "user"
-BOT_ROLE = "bot"
+BOT_ROLE = "assistant"
+
+
+def add_message(role, content):
+    st.session_state['messages'].append({"role": role, "content": content})
+    st.session_state['block'].message_handler.add_message(role, content)
+
+
+def stream_response():
+    message_placeholder = st.empty()
+    full_response = ""
+    for response in st.session_state['block'].completion_handler.create_completion(st.session_state['block']):
+        full_response += response.choices[0].delta.get("content", "")
+        message_placeholder.markdown(full_response + "▌")
+    message_placeholder.markdown(full_response)
+    return full_response
 
 st.title(PAGE_TITLE)
 
 if 'block' not in st.session_state:
-    st.session_state['block'] = block_factory.get("chat")
+    st.session_state['block'] = block_factory.get("chat", stream=True)
 
 if 'messages' not in st.session_state:
     st.session_state.messages = []
@@ -17,14 +32,13 @@ for message in st.session_state.messages:
     with st.chat_message(message['role']):
         st.markdown(message['content'])
 
-if prompt := st.chat_input("What is up?"):
-    st.session_state['messages'].append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
+if query := st.chat_input("Enter your query here"):
+    add_message(USER_ROLE, query)
 
-    with st.chat_message('assistant'):
-        message_placeholder = st.empty()
-        response = st.session_state['block'].execute(prompt)
+    with st.chat_message(USER_ROLE):
+        st.markdown(query)
 
-        message_placeholder.markdown(response)
-        st.session_state['messages'].append({"role": "assistant", "content": response})
+    with st.chat_message(BOT_ROLE):
+        response = stream_response()
+    
+    add_message(BOT_ROLE, response)
