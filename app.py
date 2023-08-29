d3d30272fffd0cd21e432f1a2d2ff58a95e5da1d
voynow <voynow99@gmail.com>
2023-08-28 20:36:51 -0400

    Quick rewrite for an even better design

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from llm_blocks import block_factory
 
 # Constants and Config
-PAGE_TITLE = "🧪 LeetLearn.ai"
+PAGE_TITLE = "LeetLearn.ai"
 USER_ROLE = "user"
 BOT_ROLE = "assistant"
 OPTIONS = [
@@ -16,39 +16,64 @@ OPTIONS = [
 ]
 
 
-def set_page_config():
+def initialize_app():
     st.set_page_config(
-        page_title="LeetLearn.ai",
+        page_title=PAGE_TITLE,
         page_icon="🧪",
         layout="wide",
         initial_sidebar_state="expanded",
-        menu_items={
-            "Get Help": "https://www.extremelycoolapp.com/help",
-            "Report a bug": "https://www.extremelycoolapp.com/bug",
-            "About": "# This is a header. This is an *extremely* cool app!",
-        },
     )
+    if "show_chat" not in st.session_state:
+        st.session_state.show_chat = False
+    if "block" not in st.session_state:
+        st.session_state["block"] = block_factory.get("chat", stream=True)
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
 
 
-def show_landing_page():
-    st.title("Welcome to LeetLearn.ai")
-    st.write(
-        "Here, you can interact with our advanced AI to get insights, help, and much more."
-    )
+def show_page():
+    if st.session_state.show_chat:
+        display_chat_interface()
+    else:
+        display_landing_page()
+
+
+def display_landing_page():
+    st.title("Welcome to LeetLearn.ai 🧪")
+    st.write("Interact with our advanced AI for insights, help, and more.")
     if st.button("Proceed to Chat"):
         st.session_state.show_chat = True
         st.experimental_rerun()
 
 
-def init_session_state():
-    if "block" not in st.session_state:
-        st.session_state["block"] = block_factory.get("chat", stream=True)
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
-        add_message(
-            BOT_ROLE,
-            "Hi, I'm LeetLearn's AI assistant. What can I help you with today?",
-        )
+def display_chat_interface():
+    st.title(PAGE_TITLE)
+    setup_sidebar()
+    display_messages()
+    user_input = st.chat_input("Enter your query here")
+    if user_input:
+        handle_chat(user_input)
+
+
+def setup_sidebar():
+    st.sidebar.title("Options Dropdown")
+    selected_option = st.sidebar.selectbox("Choose an option:", OPTIONS)
+    return selected_option
+
+
+def display_messages():
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+
+def handle_chat(query):
+    add_message(USER_ROLE, query)
+    with st.chat_message(USER_ROLE):
+        st.markdown(query)
+    with st.chat_message(BOT_ROLE):
+        response = stream_response()
+    add_message(BOT_ROLE, response)
 
 
 def add_message(role, content):
@@ -68,40 +93,6 @@ def stream_response():
     return full_response
 
 
-def render_chat():
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-
-
-def setup_sidebar():
-    st.sidebar.title("Options Dropdown")
-    selected_option = st.sidebar.selectbox("Choose an option:", OPTIONS)
-    return selected_option
-
-
-def main():
-    set_page_config()
-    init_session_state()
-
-    if "show_chat" not in st.session_state:
-        st.session_state.show_chat = False
-
-    if st.session_state.show_chat:
-        st.title(PAGE_TITLE)
-        selected_option = setup_sidebar()
-        render_chat()
-
-        if query := st.chat_input("Enter your query here"):
-            add_message(USER_ROLE, query)
-            with st.chat_message(USER_ROLE):
-                st.markdown(query)
-            with st.chat_message(BOT_ROLE):
-                response = stream_response()
-            add_message(BOT_ROLE, response)
-    else:
-        show_landing_page()
-
-
 if __name__ == "__main__":
-    main()
+    initialize_app()
+    show_page()
