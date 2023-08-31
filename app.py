6b9030183b79d265c51f3718696428187edb2ecd
voynow <voynow99@gmail.com>
2023-08-30 20:39:35 -0400

    Dropdown text inserted into chat

--- app.py
+++ app.py
@@ -1,119 +1,137 @@
-import logging
 import json
+import logging
 import streamlit as st
-
 from llm_blocks import block_factory, blocks
 
-logging.basicConfig(level=logging.INFO)
+PAGE_TITLE = "LeetLearn.ai"
+USER_ROLE = "user"
+BOT_ROLE = "assistant"
 
 SYS_MESSAGE = """
-Your system message here.
+You are a LeetCode interview tutor, mirroring the challenging yet encouraging environment of elite tech firms like Google, Amazon, etc.
+Be Concise: Stay focused and succinct to optimize learning speed.
+Adapt and Flex: Handle both structured and unstructured questions, and tailor your approach to the learner's needs.
+Encourage Reflection: Offer feedback that emphasizes growth and understanding.
+Preserve Autonomy: Allow users to ponder solutions first. Be VERY conservative with hints to foster independent thinking.
+Never Reveal Answers: Challenge and support, but don't take away the opportunity to learn.
 """
 
-# Singleton class to manage session state
-class SessionState:
-    instance = None
+solutions = json.loads(open("data/solutions.json").read())
+logging.basicConfig(level=logging.INFO)
 
-    def __new__(cls):
-        if cls.instance is None:
-            cls.instance = super(SessionState, cls).__new__(cls)
-            cls.instance.initialize_state()
-        return cls.instance
 
-    def initialize_state(self):
-        initial_state = {
-            "show_chat": False,
-            "messages": [],
-            "last_selected": None,
-            "block": block_factory.get("chat", stream=True, system_message=SYS_MESSAGE),
-        }
-        for key, value in initial_state.items():
-            if key not in st.session_state:
-                if key not in ["messages", "block"]:
-                    logging.info(f"Initializing state: {key}={value}")
-                st.session_state[key] = value
+def initialize_app():
+    st.set_page_config(
+        page_title=PAGE_TITLE,
+        page_icon="🧪",
+        initial_sidebar_state="expanded",
+    )
+    if "show_chat" not in st.session_state:
+        st.session_state.show_chat = False
+    if "block" not in st.session_state:
+        st.session_state["block"] = block_factory.get("chat", stream=True)
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+    if "current_selection" not in st.session_state:
+        st.session_state.current_selection = None
 
-# Model for handling messages and session state
-class MessageModel:
-    def __init__(self):
-        self.state = SessionState()
 
-    def add_message(self, role, content):
-        logging.info(f"Adding message from {role}: {content[:50]}...")
-        st.session_state.messages.append({"role": role, "content": content})
-        st.session_state.block.message_handler.add_message(role, content)
+def add_message(role, content):
+    st.session_state["messages"].append({"role": role, "content": content})
+    st.session_state["block"].message_handler.add_message(role, content)
 
-# Controller to connect the Model and the View
-class ChatController:
-    def __init__(self):
-        self.model = MessageModel()
 
-    def handle_user_input(self, user_input, message_placeholder):
-        self.model.add_message("user", user_input)
-        response = self.stream_response(message_placeholder)
-        self.model.add_message("assistant", response)
+def introduce_ai():
+    intro_message = "Hello! I am LeetLearn AI. What are we working on today?"
+    add_message(BOT_ROLE, intro_message)
 
-    def stream_response(self, message_placeholder):
-        full_response = ""
-        for response in st.session_state.block.completion_handler.create_completion(
-            st.session_state.block
-        ):
-            full_response += response.choices[0].delta.get("content", "")
-            message_placeholder.markdown(full_response + "▌")
-        message_placeholder.markdown(full_response)
-        return full_response
 
 def display_landing_page():
-    logging.info("Displaying landing page...")
     st.title("Welcome to LeetLearn.ai 🧪")
+    st.write("Interact with our advanced AI for insights, help, and more.")
     api_key = st.text_input("OpenAI API key:", type="password")
     if api_key:
         blocks.set_api_key(api_key)
+        st.session_state["api_key"] = api_key
+        introduce_ai()
         st.session_state.show_chat = True
         st.experimental_rerun()
 
-def manage_messages():
-    message_placeholders = []
-    for _ in st.session_state.messages:
-        message_placeholders.append(st.empty())
-    for idx, message in enumerate(st.session_state.messages):
-        with message_placeholders[idx].chat_message(message["role"]):
-            message_placeholders[idx].markdown(message["content"])
-    return message_placeholders
 
-def solutions_interface(solutions, controller):
-    options = ["Select an option..."] + solutions["name"]
-    selected_option = st.sidebar.selectbox("Choose an option:", options)
-    if selected_option != "Select an option..." and selected_option != st.session_state.last_selected:
-        st.session_state.last_selected = selected_option
-        assistant_message_placeholder = st.empty()
-        with assistant_message_placeholder.chat_message("assistant"):
-            assistant_message_placeholder.markdown("...")
-        controller.handle_user_input(selected_option, assistant_message_placeholder)
+def stream_response():
+    """
+    This streaming mechanism is critical to the application and has been
+    developed specifically to align with streamlit
+    """
+    message_placeholder = st.empty()
+    full_response = ""
+    for response in st.session_state["block"].completion_handler.create_completion(
+        st.session_state["block"]
+    ):
+        full_response += response.choices[0].delta.get("content", "")
+        message_placeholder.markdown(full_response + "▌")
+    message_placeholder.markdown(full_response)
+    return full_response
 
-def display_chat_interface(solutions, controller):
-    logging.info("Displaying chat interface...")
-    st.title("LeetLearn.ai")
-    solutions_interface(solutions, controller)
-    manage_messages()
+
+def setup_sidebar():
+    st.sidebar.title("Options Dropdown")
+    options = [None] + solutions["name"]
+    selected_option = st.sidebar.selectbox(
+        label="Choose an option:", options=options, key='selection'
+    )
+    return selected_option
+
+
+def display_messages():
+    logging.info(f"Displaying {len(st.session_state.messages)} messages")
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+
+def handle_chat(query):
+    add_message(USER_ROLE, query)
+    with st.chat_message(USER_ROLE):
+        st.markdown(query)
+    with st.chat_message(BOT_ROLE):
+        response = stream_response()
+    add_message(BOT_ROLE, response)
+
+
+def handle_selected_option(selected_option):
+    st.session_state.current_selection = selected_option
+    logging.info(f"Selected option: {selected_option}")
+    handle_chat(selected_option)
+    st.experimental_rerun()
+
+
+def handle_user_input():
     user_input = st.chat_input("Enter your query here")
-    
     if user_input:
-        assistant_message_placeholder = st.empty()
-        with assistant_message_placeholder.chat_message("assistant"):
-            assistant_message_placeholder.markdown("...")
-        controller.handle_user_input(user_input, assistant_message_placeholder)
+        logging.info(f"User input: {user_input}")
+        handle_chat(user_input)
 
-def run_app():
-    session_state = SessionState()
-    controller = ChatController()
 
-    solutions = json.loads(open("data/solutions.json").read())
+def display_chat_interface():
+    st.title(PAGE_TITLE)
+    display_messages()
+    selected_option = setup_sidebar()
 
+    selection_change = st.session_state.current_selection != selected_option
+    if selected_option is not None and selection_change:
+        handle_selected_option(selected_option)
+    else:
+        handle_user_input()
+
+
+def show_page():
     if st.session_state.show_chat:
-        display_chat_interface(solutions, controller)
+        display_chat_interface()
     else:
         display_landing_page()
 
+
 if __name__ == "__main__":
-    run_app()
+    initialize_app()
+    show_page()
