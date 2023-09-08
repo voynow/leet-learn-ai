486048c311654756aac43e1e57d475fd04fb1fe8
voynow <voynow99@gmail.com>
2023-09-08 09:29:36 -0400

    Prompt engineering

--- src/app.py
+++ src/app.py
@@ -10,10 +10,19 @@ PAGE_TITLE = "LeetLearn.ai"
 USER_ROLE = "user"
 BOT_ROLE = "assistant"
 
-SYS_MESSAGE = """Role: You are a LeetCode interview tutor, mirroring the challenging yet encouraging environment of elite tech firms like Google, Amazon, etc.
-Be Concise: Stay focused and be concise. The user's time is extremely valuable and every word you respond with takes some of the user's time.
-Never Reveal Answers: Be very vey conservative with hints, never reveal answers, be as vague as possible.
-Important: When the user shares a problem, let them think through the problem before providing hints.
+SYS_MESSAGE = """Role: You're a LeetCode interview tutor, mirroring the challenging environment of big tech (Google, Amazon, etc.)
+Be Concise: Stay focused. Be as concise as possible unless the user says otherwise.
+Never Reveal Answers: It is critical that you never give away the answer. This is a learning tool, not a cheat sheet.
+Don't give hints: Ask questions that will help the user get to the answer on their own. When a user asks for a hint, try your best not to give the answer away.
+
+Example:
+
+(LeetCode problem median-of-two-sorted-arrays)
+...
+user: Can you combine them first?
+assistant: You could, but merging the two arrays would take O(m+n) time, which doesn't meet the requirement of O(log(m+n)) time complexity. Instead, consider how binary search could be used in this problem. How might you apply it to find the median?
+
+This example highlights a bad response. It was good, until the assistant gave away that you need binary search.
 """
 
 API_KEY_ERR_MSG = (
@@ -136,7 +145,7 @@ def display_messages():
     logging.info(f"Displaying {len(st.session_state.messages)} messages")
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+            st.markdown(message["content"], unsafe_allow_html=True)
 
 
 def construct_chat_input(selected_option):
